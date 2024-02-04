bd5fa40a2dce0b08963f6c18c3a2147f18137ebb
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-02-04 18:41:57 +0000

    0::|

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,18 +5,20 @@ from os import getenv
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
+'''The Flask web application.'''
 app_host = getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """Close the current SQLAlchemy session."""
+    """Close the session"""
     storage.close()
 
 
