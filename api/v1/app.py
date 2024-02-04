7c8ab4a25a312d50c9f9d670650142fa24e1b73e
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-02-04 19:13:21 +0000

    debug app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import getenv
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Close db"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error - Not Found"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
