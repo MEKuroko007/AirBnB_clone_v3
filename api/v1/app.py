3bf0af92f47d3438cfdbcf618113c73ddaef5f64
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-02-04 17:19:40 +0000

    fix issue of cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,41 +4,37 @@ from flask import Flask
 from os import getenv
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, make_response
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(getenv('HBNB_API_PORT', '5000'))
+
+
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def closeDB(exception):
-    """Close the current SQLAlchemy session."""
+    """Close db"""
     storage.close()
 
 
 @app.errorhandler(404)
 def error_404(error):
     '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.errorhandler(400)
-def error_400_handler(error):
-    '''Handles the 400 HTTP error code.'''
-    message = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        message = error.description
-    return jsonify(error=message), 400
-
-
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
